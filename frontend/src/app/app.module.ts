32108e5086d592e5b574e642e48f28956d4cdd2e
SteliosAlegkakis <stelios.alegak@gmail.com>
2023-11-20 17:51:45 +0200

    implemented nav-bar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { environment } from 'src/environments/environment';
 import { ItemShopComponent } from './pages/item-shop/item-shop.component';
 import { ItemPreviewComponent } from './pages/item-shop/item-preview/item-preview.component';
+import { NavBarComponent } from './pages/components/nav-bar/nav-bar.component';
 
 const socketIoConfig: SocketIoConfig = { url: environment.host, options: {} };
 @NgModule({
@@ -15,7 +16,8 @@ const socketIoConfig: SocketIoConfig = { url: environment.host, options: {} };
     AppComponent,
     TasksComponent,
     ItemShopComponent,
-    ItemPreviewComponent
+    ItemPreviewComponent,
+    NavBarComponent
   ],
   imports: [
     SocketIoModule.forRoot(socketIoConfig),
