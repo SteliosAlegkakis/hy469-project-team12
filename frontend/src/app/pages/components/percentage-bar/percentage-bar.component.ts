1563137d3f3a47a0c08474b31992ca8dd57c4843
SteliosAlegkakis <stelios.alegak@gmail.com>
2023-11-29 18:53:57 +0200

    implemented percentage-bar component

---
+++ frontend/src/app/pages/components/percentage-bar/percentage-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-percentage-bar',
+  templateUrl: './percentage-bar.component.html',
+  styleUrls: ['./percentage-bar.component.scss']
+})
+export class PercentageBarComponent implements OnInit{
+  @Input() completed!: number;
+  @Input() goal!: number;
+  @Input() bar_color!: String;
+
+  percentage!:number;
+
+  ngOnInit(): void {
+    this.percentage = (this.completed/this.goal)*100;
+    this.percentage = Number(this.percentage.toFixed(0));
+
+  }
+
+}
