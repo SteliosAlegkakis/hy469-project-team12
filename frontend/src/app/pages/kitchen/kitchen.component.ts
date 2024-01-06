75878cfa79b923cddc31827eae26b27995df4c40
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-01-06 19:36:57 +0200

    added meals to kitchen page

--- frontend/src/app/pages/kitchen/kitchen.component.ts
+++ frontend/src/app/pages/kitchen/kitchen.component.ts
@@ -10,4 +10,12 @@ export class KitchenComponent {
   protein:number = 0;
   carbs:number = 0;
   fat:number = 0;
+
+  meals: {calories: number, protein:number, carbs:number, fat:number, type: string, ingredients: string, completed: boolean}[] = [
+    {calories: 400, protein: 30, carbs: 40, fat: 20, type: "Breakfast", ingredients: "eggs, avocado, bread", completed: false},
+    {calories: 500, protein: 45, carbs: 50, fat: 15, type: "Lunch", ingredients: "chicken fillet, white rice", completed: false},
+    {calories: 300, protein: 20, carbs: 20, fat: 5, type: "Afternoon snack", ingredients: "protein bar, banana", completed: false},
+    {calories: 600, protein:40, carbs: 60, fat: 20, type: "Dinner", ingredients: "spaghetti, ground beef", completed: false},
+    {calories: 300, protein:20, carbs: 30, fat: 6,  type: "Night sanck", ingredients: "yoghurt, honey, strawberries", completed: false}
+  ];
 }
