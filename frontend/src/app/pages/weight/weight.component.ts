d03ee1ae404ed93a9a47664a7ac8b693633ae311
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-01-08 22:13:27 +0200

    completed weight page view

--- frontend/src/app/pages/weight/weight.component.ts
+++ frontend/src/app/pages/weight/weight.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./weight.component.scss']
 })
 export class WeightComponent {
-
+  info: {height: Number, date: Number, month: Number}[] = [
+    {height: 70, date: 18, month: 11},
+    {height: 60, date: 19, month: 11},
+    {height: 40, date: 20, month: 11},
+    {height: 80, date: 21, month: 11},
+    {height: 30, date: 22, month: 11},
+    {height: 20, date: 23, month: 11},
+    {height: 50, date: 24, month: 11},
+    {height: 90, date: 25, month: 11},
+    {height: 60,date: 26, month: 11},
+    {height: 80, date: 27, month: 11},
+    {height: 60, date: 28, month: 11},
+    {height: 70, date: 29, month: 11},
+    {height: 100, date: 30, month: 11},
+  ]
 }
