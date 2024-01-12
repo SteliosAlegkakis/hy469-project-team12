b8e0d5c9c6b49990382af70c8ae30cc4fcf03e0b
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-01-12 13:18:22 +0200

    removed leap

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,34 +5,6 @@ import { Component, ElementRef, HostListener, Renderer2, ViewChild, OnInit } fro
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'frontend';
-
-  public GesturesStr: string[] = [
-    "SWIPE_LEFT",
-    "SWIPE_UP",
-    "SWIPE_DOWN",
-    "SWIPE_RIGHT",
-    "CIRCLE_CLOCKWISE",
-    "CIRCLE_COUNTERCLOCKWISE",
-    "PINCH",
-  ];
-
-  ngOnInit() {
-    (window as any)["electronAPI"].onGesture(
-      (arg: any) => {
-        console.log("Gesture recognised: ", this.GesturesStr[arg])
-      }
-    );
-  }
-
-  @ViewChild('appReplaceCursor', { static: true }) cursorReplacement: ElementRef = {} as ElementRef;
-
-  @HostListener('mousemove', ['$event'])
-  onmousemove(event: MouseEvent) {
-    if (this.cursorReplacement.nativeElement) {
-      this.cursorReplacement.nativeElement.style.left = event.pageX + 'px';
-      this.cursorReplacement.nativeElement.style.top = event.pageY + 'px';
-    }
-  }
 }
